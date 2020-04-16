5291bf8942a2d242231204a8b7be6cc7e94f0960
Eyal Keren <eyal.keren@gm.com>
2020-04-16 15:42:52 +0300

    Continue working on canSniffer script

--- raspberry_ws/src/wizzybug_control/src/canSniffer.py
+++ raspberry_ws/src/wizzybug_control/src/canSniffer.py
@@ -1,9 +1,22 @@
 import can
 import os
 import struct
+import logging
 
 INTERFACE = 'can0'
 BITRATE = 105263
+
+# empirically measured linear velocity in m/s
+MAX_VELOCITY = 0.5
+
+# empirically measured angular velocity in degrees/s
+MAX_ANGULAR_VELOCITY = 40.0
+
+# normalizaton constant (max value of reading)
+MAX_VALUE = 100.0
+
+def sign(x):
+  return 1-(x<=0)
 
 def can_recv(iface):
     bus = can.interface.Bus(bustype='socketcan', channel=iface)
@@ -16,8 +29,17 @@ def can_recv(iface):
         elif len(msg.data) == 4:
             data_array = struct.unpack('>4b', bytes(msg.data))
             x = data_array[2]
-            print('{},{}'.format(x,y))
 
+            # normalize according to measured joystick characteristics
+            x, y = min(abs(x), MAX_VALUE) * sign(x), min(abs(y), MAX_VALUE) * sign(y)
+
+            # notice sign flip to make forward motion positive
+            velocity = (y / MAX_VALUE) * MAX_VELOCITY
+
+            # angular velocity
+            angular_velocity = (x / MAX_VALUE) * MAX_ANGULAR_VELOCITY
+
+            logging.debug('vel: {}, ang: {}'.format(velocity, angular_velocity))
 
 def disable_iface(iface):
     os.system('sudo ip link set down {}'.format(iface))
@@ -31,4 +53,9 @@ def find_bitrate(iface):
     can_recv(iface)
 
 if __name__== "__main__":
+
+    # set logging level
+    logging.basicConfig(level=logging.DEBUG)
+
+    # run
     find_bitrate(INTERFACE)
