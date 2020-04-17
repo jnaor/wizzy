250d3b25e6462b424628d9e1fa7650381e941813
Jonathan Naor <jonathan.naor@mail.huji.ac.il>
2020-04-17 12:56:19 +0000

    trying new RDS setup

---
+++ clean.sh
@@ -0,0 +1,8 @@
+rm -rf /home/user/wizzy/raspberry_ws/build
+rm -rf /home/user/wizzy/raspberry_ws/devel
+
+rm -rf /home/user/wizzy/jetson_ws/build
+rm -rf /home/user/wizzy/jetson_ws/devel
+
+rm -rf /home/user/wizzy/simulation_ws/build
+rm -rf /home/user/wizzy/simulation_ws/devel
--- make.sh
+++ make.sh
@@ -1,12 +1,17 @@
-cd jetson_ws
-catkin_make
-cd ../raspberry_ws
-catkin_make
+cd /home/user/wizzy/raspberry_ws
+catkin init
+catkin build
+
+cd ../jetson_ws
+catkin config --init --extend /home/user/wizzy/raspberry_ws
+catkin build
+
 cd ../simulation_ws
-catkin_make
+catkin config --init --extend /home/user/wizzy/jetson_ws
+catkin build
 cd ..
 
-source jetson_ws/devel/setup.bash
+source simulation_ws/devel/setup.bash
 
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/user/wizzy/raspberry_ws/src:/home/user/wizzy/simulation_ws/src
-echo "added to ros package path"
+#export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/user/wizzy/raspberry_ws/src:/home/user/wizzy/simulation_ws/src
+#echo "added to ros package path"
