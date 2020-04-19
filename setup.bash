4d209ffa1f2552e9aef057b87ea1538297ff6137
Jonathan Naor <jonathan.naor@gm.com>
2020-04-19 10:44:43 +0300

    relative paths in setup

--- setup.bash
+++ setup.bash
@@ -1,15 +1,15 @@
-cd /home/user/wizzy/raspberry_ws
+cd raspberry_ws
 catkin config --init --extend /opt/ros/melodic
 catkin build
 source devel/setup.bash
 
 cd ../jetson_ws
-catkin config --init --extend /home/user/wizzy/raspberry_ws
+catkin config --init --extend ../raspberry_ws
 catkin build
 source devel/setup.bash
 
 cd ../simulation_ws
-catkin config --init --extend /home/user/wizzy/jetson_ws
+catkin config --init --extend ../jetson_ws
 catkin build
 source devel/setup.bash
 cd ..
