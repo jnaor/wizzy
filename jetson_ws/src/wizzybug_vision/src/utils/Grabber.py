eb821498ba75f8b8b1f38d684b013de181116cc9
Jonathan Naor <jonathan.naor@mail.huji.ac.il>
2020-05-08 11:40:22 +0300

    preparing for realsense test

--- jetson_ws/src/wizzybug_vision/src/utils/Grabber.py
+++ jetson_ws/src/wizzybug_vision/src/utils/Grabber.py
@@ -5,30 +5,39 @@ import cv2
 import sys
 import os
 
-# PACKAGE_PARENT = '..'
-# SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
-# sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-# from ..wizzy_vision import DEPTH_WIDTH, DEPTH_HEIGHT
-
-DEPTH_WIDTH, DEPTH_HEIGHT = 640, 480
-
 
 class Grabber(object):
-    """ read depth and color from file or stream """
-    def __init__(self):
+
+    def __init__(self, cam_serial, width, height, framerate):
+        # save parameters
+        self.cam_serial, self.width, self.height, self.framerate = cam_serial, width, height, framerate
+        
         # create pipeline
         self.pipeline = rs.pipeline()
 
         # create a config object
         self.config = rs.config()
 
-        # Create colorizer object
-        self.colorizer = rs.colorizer()
+        # enable device
+        self.config.enable_device(cam_serial)
 
-    def start(self, record_filename=None):
-        # configure the pipeline to stream the depth stream
-        self.config.enable_stream(rs.stream.depth, DEPTH_WIDTH, DEPTH_HEIGHT, rs.format.z16, 30)
-        self.config.enable_stream(rs.stream.color, DEPTH_WIDTH, DEPTH_HEIGHT, rs.format.rgb8, 30)
+        # # Create colorizer object
+        # self.colorizer = rs.colorizer()
+
+        # to mark which streams are to be grabbed
+        self.depth, self.color = False, False
+
+    def start(self, depth=True, color=True, record_filename=None):
+        # remember what we're streaming
+        self.depth, self.color = depth, color
+
+        if depth:
+            self.config.enable_stream(rs.stream.depth, self.width, self.height,
+                                      rs.format.z16, self.framerate)
+        
+        if color:
+            self.config.enable_stream(rs.stream.color, self.width, self.height,
+                                      rs.format.rgb8, self.framerate)
 
         if record_filename is not None:
             self.config.enable_record_to_file(record_filename)
@@ -36,24 +45,38 @@ class Grabber(object):
         # Start streaming from file
         self.pipeline.start(self.config)
 
+        os.mkdir('recording/')
+
     def grab(self):
         """ return depth and color """
 
-        # Get frameset of depth
+        # to hold result
+        result = dict()
+
+        # Get frames
         frames = self.pipeline.wait_for_frames()
 
         # Get depth frame
-        depth_frame = frames.get_depth_frame()
+        if self.depth:
+            depth_frame = frames.get_depth_frame()
+            result["depth"] = np.asanyarray(depth_frame.get_data())
 
         # Get color frame
-        color_frame = frames.get_color_frame()
+        if self.color:
+            color_frame = frames.get_color_frame()
+            result["color"] = np.asanyarray(color_frame.get_data())
 
-        # Colorize depth frame to jet colormap
-        depth_color_frame = self.colorizer.colorize(depth_frame)
+        return result
 
-        # Convert to numpy arrays
-        return np.asanyarray(depth_frame.get_data()), np.asanyarray(color_frame.get_data()), \
-               np.asanyarray(depth_color_frame.get_data())
+        # # Colorize depth frame to jet colormap
+        # depth_color_frame = self.colorizer.colorize(depth_frame)
+
+        # # Convert to numpy arrays
+        # return np.asanyarray(depth_frame.get_data()), np.asanyarray(color_frame.get_data()), \
+        #        np.asanyarray(depth_color_frame.get_data())
+
+    def stop(self):
+        self.pipeline.stop()
 
 
 class FileGrabber(Grabber):
@@ -65,15 +88,55 @@ class FileGrabber(Grabber):
         rs.config.enable_device_from_file(self.config, filename)
 
 
-def start_cameras():
+def start_cameras(width, height, framerate, depth=False, color=True):
+    # realsense context
     realsense_ctx = rs.context()
-    connected_devices = []
-    for i in range(len(realsense_ctx.devices)):
-        detected_camera = realsense_ctx.devices[i].get_info(rs.camera_info.serial_number)
-        connected_devices.append(detected_camera)
 
-        # see: https://github.com/IntelRealSense/librealsense/issues/1735
+    # initialize grabbers
+    grabbers = dict()
 
+    # iterate over connected devices
+    for cam_serial in range(len(realsense_ctx.devices)):
+
+        # current serial number
+        cam_serial = realsense_ctx.devices[cam_serial].get_info(rs.camera_info.serial_number)
+
+        grabbers[cam_serial] = Grabber(cam_serial=cam_serial,
+                                       width=width, height=height, framerate=framerate)
+        # start streams
+        grabbers[cam_serial].start(depth=depth, color=color)
+
+        # TODO: remove this
+        os.mkdir(f'recording/{cam_serial}')
+        if depth:
+            os.mkdir(f'recording/{cam_serial}/depth')
+        if color:
+            os.mkdir(f'recording/{cam_serial}/color')
+
+    # counter at zero for each camera
+    counter = {serial: 0 for serial in grabbers.keys()}
+
+    while True:
+        for cam_serial, grabber in grabbers.items():
+            # get current images
+            images = grabber.grab()
+
+            # show images
+            for name, img in images.items():
+                #cv2.imshow(f"serial {cam_serial} stream {name}", img)
+                cv2.imwrite(f"recording/{cam_serial}/{name}/{counter[cam_serial]:4}.png", img)
+                counter[cam_serial] += 1
+
+            #cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    start_cameras(width=640, height=480, framerate=15)
+    os.mkdir("recording")
+    os.mkdir("recording/color")
+    os.mkdir("recording/depth")
+
+    print("helllloooo")
 
 
 
