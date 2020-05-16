51f3e701cf6a3abbf3914a3c3b77727ba1801f02
Sharon Fraiman <sharon.fraiman@gm.com>
2020-05-16 21:54:17 +0300

    flic button initial commit

---
+++ raspberry_ws/src/wizzybug_control/src/flic_button/flic_client/wizzy_btn_client.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+Wizzy Button Client
+===================
+Based on file test_client.py
+This program attempts to connect to all previously verified Flic buttons by this server.
+"""
+
+import fliclib
+
+import logging
+log = logging.getLogger(__name__)
+
+def got_button(bd_addr):
+    cc = fliclib.ButtonConnectionChannel(bd_addr)
+    cc.on_button_single_or_double_click_or_hold = button_cb
+    cc.on_connection_status_changed = on_connection_status_changed_cb
+    client.add_connection_channel(cc)
+
+def init_buttons(items):
+    log.debug("*** INIT_BUTTONS")  
+    log.debug(items)
+    for bd_addr in items["bd_addr_of_verified_buttons"]:
+        got_button(bd_addr)
+
+def button_cb (channel, click_type, was_queued, time_diff) : 
+    log.debug(channel.bd_addr + " " + str(click_type))
+    if click_type.name == "ButtonSingleClick" :
+        print ("*** Once !")
+    elif click_type.name == "ButtonDoubleClick" :
+        print ("*** Twice !")
+    elif click_type.name == "ButtonHold" :
+        print ("*** Hold !")
+        
+def on_connection_status_changed_cb(channel, connection_status, disconnect_reason) :
+    print(channel.bd_addr + " " + str(connection_status) + \
+          (" " + str(disconnect_reason) if connection_status == fliclib.ConnectionStatus.Disconnected else ""))
+    
+    
+if __name__ == "__main__" :
+   log.error ("*** Init flick client")
+   client = fliclib.FlicClient("localhost")
+   # Initialize buttons with the pre-paired buttons in the bin directory, wizzy_buttons.db file
+   client.get_info(init_buttons)
+   client.handle_events()
---
+++ raspberry_ws/src/wizzybug_control/src/flic_button/flic_server/readme.txt
@@ -0,0 +1,2 @@
+flic server is based on the Github project :
+https://github.com/50ButtonsEach/fliclib-linux-hci
