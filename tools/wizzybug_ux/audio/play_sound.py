2a297dabb40585b61240e16e531318459f209078
Sharon Fraiman <sharon.fraiman@gm.com>
2020-05-17 22:59:07 +0300

    Connect sound to ROS and add package "playsound". Requires however sudo to hear the sound file, but this causes a path error in ROS. So currently does not work.

--- tools/wizzybug_ux/audio/play_sound.py
+++ tools/wizzybug_ux/audio/play_sound.py
@@ -1,14 +1,35 @@
-import os
-
-def play_wav(wav):
-	if wav.endswith('.wav'):
-		os.system('aplay ' + wav)
-
-def play_B5():
-	play_wav(os.getcwd()+'/B5.wav')
+# import os
 
 
-def play_Alert():
-        play_wav(os.getcwd()+'/Alert_C1_Trimmed.wav')
+import sys
+sys.path.append('/opt/ros/melodic/lib/python2.7/dist-packages')
+import rospy
+from std_msgs.msg import String
+
+from playsound import playsound
+
+# def play_wav(wav):
+	# if wav.endswith('.wav'):
+		# os.system('aplay ' + wav)
+
+# def play_B5():
+	# play_wav(os.getcwd()+'/B5.wav')
 
 
+# def play_Alert():
+        # play_wav(os.getcwd()+'/Alert_C1_Trimmed.wav')
+
+def play_bell() :
+    playsound("./ding_dong.mp3")
+        
+def sound_cb(msg): 
+    print ("*** Got" + str(msg))            
+    if "ButtonSingleClick" in str(msg) :
+        print ("*** Ringing the bell")
+        play_bell()
+        
+print ("Initing sound")        
+rospy.init_node('play_sound', anonymous=True)
+# Subscribe 
+rospy.Subscriber('/wizzy/flic_btn', String, sound_cb)
+rospy.spin()
