94f23bb61c9b035425bc8e2d12f7e216ff23b927
Eyal Keren <eyal.keren@gm.com>
2020-07-01 17:34:19 +0300

    adding sound to launch

--- raspberry_ws/src/wizzybug_hmi/src/play_sound.py
+++ raspberry_ws/src/wizzybug_hmi/src/play_sound.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
 
@@ -17,6 +19,10 @@ def sound_cb(msg):
 
 rospy.loginfo("Initializing audio")
 rospy.init_node('play_sound', anonymous=True)
+
+# read camera configuration. default to local if no parameter set by ros
+bell_sound_file = rospy.get_param('bell_sound', '../sound/ding_dong.mp3')
+rospy.loginfo('using bell sound file {}'.format(bell_sound_file))
 
 # Subscribe
 rospy.Subscriber('/wizzy/flic_btn', String, sound_cb)
