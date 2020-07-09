03e79ec627aac4bdc67d1e6358aed70df136903b
Eyal Keren <eyal.keren@gm.com>
2020-07-09 11:37:06 +0300

    one more non-working LEDs file

--- sudo_start.sh
+++ sudo_start.sh
@@ -1,11 +1,13 @@
+WIZZY_HOME=/home/wizzy-aux/wizzy/
+
 # for usb relay (? don't remember)
 chmod +x /dev/ttyACM0
 
 # init LED controller
-python3 wizzy_led_controller.py
+# python3 $WIZZY_HOME/raspberry_ws/src/wizzybug_hmi/src/wizzy_led_controller.py & 
 
 # FLIC button 
-FLIC_DIR=/home/wizzy-aux/wizzy/raspberry_ws/src/wizzybug_control/src/flic_button
+FLIC_DIR=$WIZZY_HOME/raspberry_ws/src/wizzybug_control/src/flic_button
 
 # start flic server
 $FLIC_DIR/flic_server/bin/armv6l/flicd -f  $FLIC_DIR/flic_server/bin/armv6l/wizzyButton.sqlite  -h hci1 &
