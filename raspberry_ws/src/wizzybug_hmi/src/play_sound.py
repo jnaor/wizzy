7378a3210a217f7af9045e0e717ca98e54206428
Eyal Keren <eyal.keren@gm.com>
2020-07-20 13:23:12 +0300

    using playdub (and ffmpeg\!) sound plays without sudo

--- raspberry_ws/src/wizzybug_hmi/src/play_sound.py
+++ raspberry_ws/src/wizzybug_hmi/src/play_sound.py
@@ -3,29 +3,23 @@
 import rospy
 from std_msgs.msg import String
 
+from pydub import AudioSegment
+from pydub.playback import play
+
 import os
-import socket
-
-HOST = "127.0.0.1"
-PORT = 6666
-
-# a workaround for pulseaudio sudo-less audio playing woes
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
 
 rospy.loginfo("Initializing audio")
 rospy.init_node('play_sound', anonymous=True)
 
 # read camera configuration. default to local if no parameter set by ros
-bell_sound_file = rospy.get_param('bell_sound', 'sound/ding_dong.mp3')
+bell_sound_file = rospy.get_param('bell_sound', 'sound/B5.wav')
 
 # get absolute path
 bell_sound_file = os.path.abspath(bell_sound_file)
 rospy.loginfo('using bell sound file {}'.format(bell_sound_file))
 
-def play_bell():
-    print("sending")
-    sock.sendall(bell_sound_file.encode())
+# read sound file (one time)
+bell_sound = AudioSegment.from_file(bell_sound_file)
 
 def sound_cb(msg):
     """
@@ -35,7 +29,7 @@ def sound_cb(msg):
     rospy.loginfo('play_sound received message {}'.format(msg))    
     if "on" in str(msg):
         rospy.loginfo("*** Got USB Relay ON : Ringing the bell")
-        play_bell()
+        play(bell_sound)
 
 # Subscribe
 rospy.Subscriber("usb_relay_command", String, sound_cb)
