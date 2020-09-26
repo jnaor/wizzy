138789d0a73f036e3235fd61de39ce1e753723d2
Jonathan Naor <jonathan.naor@mail.huji.ac.il>
2020-09-26 12:16:17 +0000

    camera part in (but untested). moving around to search for the right place in directory structure

--- simulation_ws/src/wizzybug_simulation/src/sensing_visualization.py
+++ simulation_ws/src/wizzybug_simulation/src/sensing_visualization.py
@@ -4,44 +4,77 @@ from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Vector3
 from wizzybug_msgs.msg import lidar_data, obstacle, obstacleArray
 
+# visualization frequency detemined by this
+SLEEP_DURATION = 0.05
+
+# for how long to display markers
 MARKER_DISPLAY_DURATION = 0.1
+
 
 class Visualizer(object):
 
     def __init__(self, marker_display_duration):
         
+        # initialize node
+        rospy.init_node('sensing_visualization', log_level=rospy.DEBUG)
+
         # initialize display duration for markers
         self.display_duration = rospy.duration(MARKER_DISPLAY_DURATION)
 
-        # initialize node
-        rospy.init_node('sensing_visualization')
-
         # subscribe to lidar
-        lidar_subscriber = rospy.Subscriber('/wizzy/lidar_proc', lidar_data, handler.activation_callback)
+        lidar_subscriber = rospy.Subscriber('/wizzy/lidar_proc', lidar_data, self.lidar_callback)
         
-        # subscrive to camera results
-        camera_subscriber = rospy.Subscriber('/wizzy/obstacle_list', obstacleArray, handler.activation_callback)
+        # subscribe to camera results
+        camera_subscriber = rospy.Subscriber('/wizzy/obstacle_list', obstacleArray, self.camera_callback)
 
         # markers publisher
-        self.obstacles_publisher = rospy.Publisher('obstacle_markers', MarkerArray, queue_size = 10)
-
-        # to hold obstacle markers array
-        self.obstacle_markers = []
-
+        self.obstacles_publisher = rospy.Publisher('obstacle_marker', MarkerArray, queue_size = 10)
 
     def lidar_callback(self, lidar_data):
+        rospy.logdebug('in lidar callback')
         pass
 
-    def camera_callback(self, obstacleArray):
-        pass
+    def camera_callback(self, obstacle_array):
+        rospy.logdebug('in camera callback')
 
-    def run():
-        while not rospy.is_shutdown():
-            pass
+        # to hold obstacle markers array
+        markerArray = MarkerArray()
 
+        # go over received array and add markers per obstacle
+        for obstacle in obstacle_array:
+
+            # new marker for this obstacle
+            marker = Marker()
+
+            # marker attributes
+            marker.header.frame_id = "/obstacle_marker"
+
+            marker.type = marker.CUBE
+            marker.action = marker.ADD
+            marker.scale.x = 0.2
+            marker.scale.y = 0.2
+            marker.scale.z = 0.2
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            marker.pose.orientation.w = 1.0
+            
+            # get location from message
+            marker.pose.position.x = obstacle.x.data
+            marker.pose.position.y = obstacle.y.data
+            marker.pose.position.z = obstacle.z.data
+
+            markerArray.markers.append(marker)
+
+        # publish markers for detected 
+        self.obstacles_publisher.publish(markerArray)
+
+    def spin(self):
+        rospy.spin()
 
 if __name__ == "__main__":
 
     # initialize visualizer and run
-    Visualizer(MARKER_DISPLAY_DURATION).run()
+    Visualizer(MARKER_DISPLAY_DURATION).spin()
 
