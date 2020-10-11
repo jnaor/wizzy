d867a973f8cae54d418961e41dfe6ab9bf6c1e86
Eyal Keren <eyal.keren@gm.com>
2020-10-11 10:56:55 +0300

    removed obsolete sound server

--- sudo_start.sh
+++ sudo_start.sh
@@ -3,12 +3,6 @@ WIZZY_HOME=/home/wizzy-aux/wizzy/
 # for usb relay
 chmod +x /dev/Relay
 
-# init LED controller
-# python3 $WIZZY_HOME/raspberry_ws/src/wizzybug_hmi/src/wizzy_led_controller.py & 
-
-# initialize sound server
-python3 $WIZZY_HOME/raspberry_ws/src/wizzybug_hmi/src/sound_server.py 6666 &
-
 # FLIC button 
 FLIC_DIR=$WIZZY_HOME/raspberry_ws/src/wizzybug_control/src/flic_button
 
