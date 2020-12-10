a4ed1c49f4464f5184036442257efe28b112b666
Jonathan Naor <jonathan.naor@mail.huji.ac.il>
2020-12-10 13:58:08 +0200

    crashes jetson

---
+++ jetson_ws/src/wizzybug_vision/src/run_segnet.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python2
+
+import sys
+import jetson.inference
+import jetson.utils
+
+import argparse
+
+import rospy
+from cv_bridge import CvBridge, CvBridgeError
+
+# inputs are images
+from sensor_msgs.msg import Image
+
+# callback. do this when a new image arrives
+def callback(msg):
+
+    # get opencv image from ROS topic
+    img_input = bridge.imgmsg_to_cv2(msg)
+
+    # allocate buffers for this size image
+    buffers.Alloc(img_input.shape, img_input.format)
+
+    # process the segmentation network
+    net.Process(img_input, ignore_class=opt.ignore_class)
+
+    # generate the overlay
+    if buffers.overlay:
+        net.Overlay(buffers.overlay, filter_mode=opt.filter_mode)
+
+    # generate the mask
+    if buffers.mask:
+        net.Mask(buffers.mask, filter_mode=opt.filter_mode)
+
+    # composite the images
+    if buffers.composite:
+        jetson.utils.cudaOverlay(buffers.overlay, buffers.composite, 0, 0)
+        jetson.utils.cudaOverlay(buffers.mask, buffers.composite, buffers.overlay.width, 0)
+
+    # render the output image
+    output.Render(buffers.output)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(opt.network, net.GetNetworkFPS()))
+
+    # print out performance info
+    jetson.utils.cudaDeviceSynchronize()
+    net.PrintProfilerTimes()
+
+    # compute segmentation class stats
+    if opt.stats:
+        buffers.ComputeStats()
+
+
+if __name__ == '__main__':
+    # parse the command line
+    parser = argparse.ArgumentParser(description="Segment a live camera stream using an semantic segmentation DNN.", 
+                                    formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.segNet.Usage() +
+                                    jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+
+    parser.add_argument("--network", type=str, default="fcn-resnet18-voc", help="pre-trained model to load, see below for options")
+    parser.add_argument("--filter-mode", type=str, default="linear", choices=["point", "linear"], help="filtering mode used during visualization, options are:\n  'point' or 'linear' (default: 'linear')")
+    parser.add_argument("--visualize", type=str, default="overlay,mask", help="Visualization options (can be 'overlay' 'mask' 'overlay,mask'")
+    parser.add_argument("--ignore-class", type=str, default="void", help="optional name of class to ignore in the visualization results (default: 'void')")
+    parser.add_argument("--alpha", type=float, default=150.0, help="alpha blending value to use during overlay, between 0.0 and 255.0 (default: 150.0)")
+    parser.add_argument("--stats", action="store_true", help="compute statistics about segmentation mask class output")
+
+    is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+
+    try:
+        opt = parser.parse_known_args()[0]
+    except:
+        print("")
+        parser.print_help()
+        sys.exit(0)
+
+    # load the segmentation network
+    net = jetson.inference.segNet(opt.network, sys.argv)
+
+    # set the alpha blending value
+    net.SetOverlayAlpha(opt.alpha)
+
+    # create buffer manager
+    buffers = segmentationBuffers(net, opt)
+
+    # opencv "bridge" for incoming images
+    bridge = CvBridge()
+
+    # initialize ROS node
+    rospy.init_node('run_segnet', log_level=rospy.DEBUG)
+
+    # input topic to listen to
+    input_topic = rospy.get_param('input_topic', '/camera/color/image_raw')
+
+    rospy.loginfo('listening for input images topic {}'.format(input_topic))
+    rospy.Subscriber(input_topic, Image, callback)
+
+    # create video output
+    output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+
+    # start spinning
+    rospy.spin()
