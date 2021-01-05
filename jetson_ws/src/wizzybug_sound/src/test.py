782ee117c55e75af1d3a6a1164283ea1ed0f8031
Jonathan Naor <jonathan.naor@mail.huji.ac.il>
2021-01-05 14:18:37 +0200

    moved sound to jetson

---
+++ jetson_ws/src/wizzybug_sound/src/test.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import time
+import threading
+import rospy
+from std_msgs.msg import *
+from wizzybug_msgs.msg import *
+
+
+def index_to_mode(idx):
+    modded_idx = idx % 3
+    print(idx, modded_idx)
+    if modded_idx == 0:
+        return 'WizzyA'
+    elif modded_idx == 1:
+        return 'WizzyB'
+    elif modded_idx == 2:
+        return 'WizzyC'
+    else:
+        return 'WizzyClear'
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('hmi_tester')
+    chair_state_pub = rospy.Publisher('/chair_state', ChairState, queue_size=50)
+    time.sleep(1)  # For letting the publisher subscribe in the server
+    chair_state = ChairState()
+
+    rate = rospy.Rate(0.2)
+    count = -2     
+
+    while not rospy.is_shutdown():
+        chair_state.ttc_msg.ttc_azimuth = count * 1.047
+        chair_state.state.data = index_to_mode(count)
+        chair_state_pub.publish(chair_state)
+        print(chair_state.ttc_msg.ttc_azimuth, chair_state.state.data)
+        count += 1
+        if count == 4:
+            count = -2
+        rate.sleep()
+
+
+
+
+    
