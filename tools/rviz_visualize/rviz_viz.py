c541d11164350b6cb8099dd05a72f6dd9d1b70eb
Sharon Fraiman <sfsharon@gmail.com>
2021-02-13 20:46:07 +0200

    WIP - Add lidar processing data handling

--- tools/rviz_visualize/rviz_viz.py
+++ tools/rviz_visualize/rviz_viz.py
@@ -5,40 +5,54 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Quaternion, Pose, Point, Vector3
 from std_msgs.msg import Header, ColorRGBA
 
+from wizzybug_msgs.msg import ttc, lidar_data
+
 import math
 import time
 
 # To solve frame TF issue, need to run :
 # $ rosrun tf static_transform_publisher 0 0 0 0 0 0 1 map base_link 10
 
-def show_text_in_rviz(marker_publisher, text):
+class CallbackItems:
+    def __init__(self):
+        self.lidar_dist = 100.0
+    def lidar_dist_to_obstacle_callback(self, data):
+        self.lidar_dist = data.visible_floor_distance
+        print ("self.lidar_dist : " + str(self.lidar_dist))
+
+
+def my_main(inputs_container):
+    """
+    One place to initalize and run the code
+    """
+    # Initialize and run node	
+    rospy.init_node('my_node')
+
+    marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
+
+    lidar_dist_to_obstacle_subscriber = rospy.Subscriber('/wizzy/lidar_proc',
+                                                        lidar_data,
+                                                        inputs_container.lidar_dist_to_obstacle_callback,
+                                                        queue_size=1)
+
     marker = Marker(
-#                type=Marker.TEXT_VIEW_FACING,
-                type=Marker.SPHERE,
-                id=0,
-                action=Marker.ADD,
-                lifetime=rospy.Duration(1.5),
-                pose=Pose(Point(1,1,1), Quaternion(0, 0, 0, 1)),
-                scale=Vector3(1, 1, 1),
-                header=Header(frame_id='base_link'),
-                color=ColorRGBA(0.0, 1.0, 0.0, 0.8))
-    
-    marker_publisher.publish(marker)
+    #                type=Marker.TEXT_VIEW_FACING,
+        type=Marker.ARROW,
+        id = 123,  # Make sure the ID is different for every marker you make!
+        #   action=Marker.ADD,
+        #   lifetime=rospy.Duration(1.5),
+        pose=Pose(Point(0, -0.1, 0), Quaternion(0, 0, 0, 1)),
+        scale=Vector3(1, 1, 1),
+        header=Header(frame_id='base_link', stamp = rospy.Time.now()),
+        color=ColorRGBA(0.0, 1.0, 0.0, 0.8))
 
-def my_main():
-  """
-  One place to initalize and run the code
-  """
-  # Initialize and run node	
-  rospy.init_node('my_node')
-  marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
+# lidar_data.visible_floor_distance
 
-  cnt = 0
-  while (1) :
-    if cnt > 10 : cnt = 0
-    rospy.sleep(0.5)                                                             
-    show_text_in_rviz(marker_publisher, 'Hello world! ' + str(cnt))
-    cnt += 1
+    while not rospy.is_shutdown():
+        marker.scale.x = 2 + math.sin(time.time())
+        marker.header.stamp = rospy.Time.now()
+        marker_publisher.publish(marker)
+        rospy.sleep(0.5)
 
 def idan_main():
     # Initialize and run node
@@ -63,4 +77,6 @@ def idan_main():
         rospy.sleep(0.5)
 
 if __name__ == '__main__':
-  idan_main()
+    inputs_container = CallbackItems()
+
+    my_main(inputs_container)
