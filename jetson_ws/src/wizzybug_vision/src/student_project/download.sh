15c0aacc6b7a62ba627816785cec9b1a225dd025
Jonathan Naor <jonathan.naor@gm.com>
2023-07-27 10:00:31 +0300

    script to download network weight files

---
+++ jetson_ws/src/wizzybug_vision/src/student_project/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+wget https://github.com/PINTO0309/MobileNet-SSD-RealSense/blob/master/caffemodel/MobileNetSSD/MobileNetSSD_deploy.caffemodel
+wget https://github.com/PINTO0309/MobileNet-SSD-RealSense/blob/master/caffemodel/MobileNetSSD/MobileNetSSD_deploy.prototxt
